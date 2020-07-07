7a66e8e9ed17adaf7536922b10d45b282d7da1f1
XiaoLin <i@xiaolin.in>
2020-07-07 12:53:25 +0800

    docs(changelog): add changelog

--- .grenrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "dataSource": "commits",
-    "includeMessages": "all",
-    "changelogFilename": "CHANGELOG.md",
-    "template": {
-        commit: ({ message, url, author, name }) => `- [${message}](${url})`
-    }
-}
