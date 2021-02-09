9852ce4cc37ad9077d35386f333cb97744d2f606
Lyn <i@lyn.moe>
2021-02-09 16:36:07 +0800

    fix: config included in pre-build file

--- source/config.js
+++ source/config.js
@@ -1,6 +1,9 @@
-const configFile = require('../config')
+const fs = require('fs')
+const requireFromString = require('require-from-string')
+
+const configData = requireFromString(fs.readFileSync(process.cwd() + '/config.js').toString())
 
 module.exports = (name, defaultValue = '') => {
-  if (configFile[name]) return configFile[name]
+  if (configData[name]) return configData[name]
   else return defaultValue
 }
